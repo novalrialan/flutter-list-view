d0ef32edcd61b8f37b877246f582ce025620b536
NovalRialan <novalrialan214@gmail.com>
2023-01-05 22:42:29 +0700

    setup part1 listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,41 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-// StatelessWidget dipakai jika membuat form yang kita buat tidak ada perubahan UI
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // backgroundColor: Colors.amber,
         appBar: AppBar(
-          // widget Appbar
-          title: Text("My Apps"),
+          title: Text("List View"),
         ),
-        body: Center(
-          // widget Center
-          child: Text(
-            "AWhalodasdasdasdadlajdaksfdfdfddfdfdfdfdfdfdfdfdfdfdfdfdfdfdffsdfsdfsdfsfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfhkjhkjhhjjhkjdaasdasdsadasdasdadadajkjkjkjkjkjkjkjkkjkjjkjkjkjfkjkjkjdfsfsdfsjfksdfknsfnsf",
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              backgroundColor: Colors.black,
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 10,
-              fontFamily: "TitilliumWeb",
-              decorationStyle: TextDecorationStyle.wavy,
-              decoration: TextDecoration.lineThrough,
-              decorationColor: Colors.red,
-              decorationThickness: 5,
+        body: ListView(
+          // scrollDirection: Axis.horizontal, membuat tampilan content horizontal kesamping
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.red,
             ),
-          ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.green,
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.amber,
+            ),
+          ],
         ),
       ),
     );
