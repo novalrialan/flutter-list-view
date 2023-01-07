14c72c923d2ee7f5399d08c5c7e07a369cd667fd
NovalRialan <novalrialan214@gmail.com>
2023-01-07 13:22:12 +0700

    setup method imagewidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,31 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List Tile"),
+          title: Text("Image Widget"),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              title: Text("Noval Rialan"),
-              subtitle: Text(
-                "this is subtitle okay..asdasdasdasdasdasdasdasdasdasdsdsdsdsdsdsdsdsdsdsdsdsdsds",
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-              // tileColor: Colors.red,
-              // dense: true,
-              // onTap: () {
-              //   return;
-              // }
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text("Noval Rialan"),
-              subtitle: Text("this is subtitle okay.."),
-              leading: CircleAvatar(),
-              trailing: Text("10:00 PM"),
-            ),
-          ],
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 500,
+            color: Colors.amber,
+            child: Image.network("https://picsum.photos/200"),
+            // child: Image(
+            //   fit: BoxFit.cover,
+            //   image: AssetImage(
+            //     "images/image.jpeg",
+            //   ),
+            //   // image: NetworkImage("https://picsum.photos/200")
+            // ),
+          ),
         ),
       ),
     );
